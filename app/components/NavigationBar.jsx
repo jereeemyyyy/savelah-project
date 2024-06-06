40042eab77d76708d00308d1969f928d129f0b70
jereeemyyyy <jeremysimwenze@gmail.com>
2024-06-06 17:45:48 +0800

    Added Icons to NavigationTab

--- app/components/NavigationBar.jsx
+++ app/components/NavigationBar.jsx
@@ -4,6 +4,7 @@ import ExpensesScreen from '../screens/expenses/ExpensesScreen';
 import BudgetsScreen from '../screens/budgets/BudgetsScreen';
 import ProfileScreen from '../screens/profile/ProfileScreen';
 import LeaderBoardsScreen from '../screens/leaderboards/LeaderBoardsScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 
 export default function NavigationBar() {
@@ -12,11 +13,41 @@ export default function NavigationBar() {
 
     return (
         <Tab.Navigator screenOptions={{ headerShown: false }}> 
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Expenses" component={ExpensesScreen} />
-            <Tab.Screen name="Budgets" component={BudgetsScreen} />
-            <Tab.Screen name="LeaderBoards" component={LeaderBoardsScreen} />
-            <Tab.Screen name="Profile" component={ProfileScreen} />
+            <Tab.Screen name="Home" 
+                        component={HomeScreen}
+                        options={{
+                            tabBarIcon: ({ color, size }) => (
+                                <Ionicons name="home" color={color} size={size} />
+                            ),
+                        }} />
+            <Tab.Screen name="Expenses" 
+                        component={ExpensesScreen}
+                        options={{
+                            tabBarIcon: ({ color, size }) => (
+                                <Ionicons name="wallet" color={color} size={size} />
+                            ),
+                        }} />
+            <Tab.Screen name="Budgets" 
+                        component={BudgetsScreen} 
+                        options={{
+                            tabBarIcon: ({ color, size }) => (
+                                <Ionicons name="pie-chart" color={color} size={size} />
+                            ),
+                        }} />
+            <Tab.Screen name="LeaderBoards" 
+                        component={LeaderBoardsScreen}
+                        options={{
+                            tabBarIcon: ({ color, size }) => (
+                                <Ionicons name="trophy" color={color} size={size} />
+                            ),
+                        }} />
+            <Tab.Screen name="Profile" 
+                        component={ProfileScreen} 
+                        options={{
+                            tabBarIcon: ({ color, size }) => (
+                                <Ionicons name="person" color={color} size={size} />
+                            ),
+                        }}/>
         </Tab.Navigator>
     );
 }
