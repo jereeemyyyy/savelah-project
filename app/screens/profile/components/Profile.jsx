a87d00bd8c2b25ddcbed6a68d0f99cc32b605fe0
jereeemyyyy <jeremysimwenze@gmail.com>
2024-07-26 00:49:36 +0800

    Added edit username button in profile screen

--- app/screens/profile/components/Profile.jsx
+++ app/screens/profile/components/Profile.jsx
@@ -1,11 +1,14 @@
-import { Text, View, TouchableOpacity, Image } from 'react-native';
+import { Text, View, TouchableOpacity, Image, Modal, TextInput, Button, Alert } from 'react-native';
 import { useState, useEffect } from 'react';
 import * as ImagePicker from 'expo-image-picker';
-import { supabase } from '../../../../lib/supabase';    
+import { supabase } from '../../../../lib/supabase'; 
+import { MaterialIcons } from '@expo/vector-icons'; 
 
 export default function Profile({ userData }) {
     const [profilePicture, setProfilePicture] = useState(null);
     const [profileName, setProfileName] = useState(null);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [newProfileName, setNewProfileName] = useState('');
 
     useEffect(() => {
         if (userData) {
@@ -48,6 +51,37 @@ export default function Profile({ userData }) {
         }
     };
 
+    // Update profile name
+    const updateProfileName = async () => {
+
+        if (!newProfileName) {
+            Alert.alert('Invalid Input', 'Please enter a new profile name');
+            return;
+        }
+
+
+        try {
+            const { error } = await supabase
+                .from('profiles')
+                .update({ username: newProfileName })
+                .eq('id', userData.id);
+
+            if (error) {
+                console.error('Error updating profile:', error.message);
+            } else {
+                setProfileName(newProfileName);
+                setModalVisible(false);
+            }
+        } catch (error) {
+            console.error('Error updating profile:', error.message);
+        }
+    };
+
+    const handleCloseModal = () => {
+        setModalVisible(false);
+        setNewProfileName('');
+    };
+
     return (
         <View className="flex-row items-center mb-8">
             <TouchableOpacity onPress={pickProfilePicture}>
@@ -57,7 +91,55 @@ export default function Profile({ userData }) {
                     <View className="w-20 h-20 bg-gray-200 rounded-full mr-4" />
                 )}
             </TouchableOpacity>
-            <Text className="text-2xl font-bold text-white">{profileName}</Text>
+            <View>
+                <Text className="text-2xl font-bold text-white">{profileName}</Text>
+                <TouchableOpacity
+                    onPress={() => setModalVisible(true)}
+                    className="absolute top-4 -right-5"
+                >
+                    <MaterialIcons name="edit" size={20} color="gray" />
+                </TouchableOpacity>
+            </View>
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View className="flex-1 justify-center items-center bg-black bg-opacity-50" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+                    <View className="bg-white rounded-2xl p-10 items-center">
+                        <Text className="text-2xl font-bold mb-4">Edit Profile Name</Text>
+                        <TouchableOpacity
+                            onPress={handleCloseModal}
+                            style={{ position: 'absolute', top: 15, right: 15 }}
+                            className=""
+                          
+                        >
+                            <MaterialIcons name="cancel" size={24} color="black" />
+                        </TouchableOpacity>
+                        <TextInput
+                            value={newProfileName}
+                            onChangeText={setNewProfileName}
+                            placeholder="Enter new profile name"
+                            className="w-56 h-10 border border-gray-300 rounded px-3 mb-3"
+                        />
+                        <TouchableOpacity
+                            className="bg-purple-500 p-2 rounded my-2 w-40 items-center"
+                            onPress={updateProfileName}
+                        >
+                            <Text className="text-white text-base">Save</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            className="bg-red-500 p-2 rounded my-2 w-40 items-center"
+                            onPress={handleCloseModal}
+                        >
+                            <Text className="text-white text-base">Cancel</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
 }
